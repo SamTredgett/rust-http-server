7905447fa74c2daa84c14d5617d390e3ae32408f
SamTredgett <sam.tredgett@gmail.com>
2022-07-03 11:35:28 +0100

    Main project scaffold

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+
+fn main() {
+    let string = String::from("127.0.0.1:8080");
+    let string_slice = &string[10..];
+    dbg!(&string);
+    dbg!(string_slice);
+
+    // let server = Server::new("127.0.0.1:8080");
+    // server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String)-> Self {
+        Self { 
+            addr
+        }
+    }
+    fn run(self) {
+
+
+    }
+}
